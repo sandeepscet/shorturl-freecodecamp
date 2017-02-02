3d255dc53345e769cf0b1d62b635e9e7683fefae
Sandeep <sandeep.scet@gmail.com>
2017-02-02 19:05:02 +0000

    timestamp api code

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 var express = require('express')
 var app = express()
+var moment = require('moment');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/:datestring', function(req,res) {
+  var myDate;
+  if(/^\d{8,}$/.test(req.params.datestring)) {
+    myDate = moment(req.params.datestring, "X");
+  } else {
+    myDate = moment(req.params.datestring, "MMMM D, YYYY");
+  }
+
+  if(myDate.isValid()) {
+    res.json({
+      unix: myDate.format("X"),
+      natural: myDate.format("MMMM D, YYYY")
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
+
+
+});
+
+
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
