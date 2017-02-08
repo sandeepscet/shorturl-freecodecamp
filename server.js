5afea53d2c2b73cae142d254b7e9dddb6b28fd14
Sandeep <sandeep.scet@gmail.com>
2017-02-08 18:52:51 +0000

    working now

--- server.js
+++ server.js
@@ -3,66 +3,68 @@ var app = express()
 var mongoUtil = require('./mongoUtil.js');
 
 
-var doc = {
-  id: 1, url:'https://google.com'
-}
+app.get('/:id', function(req, res) {
+  var id = parseInt(req.params.id,10);
 
-mongoUtil.mongoInsert(doc,function(data){
-        console.log(data);
-        console.log('inserted');
-});
-
-mongoUtil.mongoFindById(1,function(data){
-        console.log(data);
-        console.log('findById');
-});
-
-mongoUtil.mongoFindByUrl('https://google.com',function(data){
-        console.log(data);
-        console.log('findByUrl');
-});
-
-mongoUtil.mongoGetLastinsertedRecored(function(data){
-        console.log(data);
-        console.log('lastInseted');
-});
-
-
-app.get('/:id', function(req,res) {
-var myDate = '';
-  if(myDate.isValid()) {
-    res.json({
-      unix: myDate.format("X"),
-      natural: myDate.format("MMMM D, YYYY")
-    });
+  if(Number.isNaN(id)) {
+    res.status(404).send("Invalid Short URL");
   } else {
-    res.json({
-      unix: null,
-      natural: null
+    mongoUtil.mongoFindById(id, function ( docs) {
+    
+      if (docs && docs.length) {
+        res.redirect(docs[0].url);
+      } else {
+        res.status(404).send("Invalid Short URL");
+      }
     });
+
   }
-
-
 });
 
+app.get('/new/*?', function(req,res) {
+  var theUrl = req.params[0];
 
-app.get('/new/*', function(req,res) {
+  // Validate the URL
+  if(theUrl) {
+    // Search for URL first
+    mongoUtil.mongoFindByUrl(theUrl, function (docs) {
+      if(docs && docs.length) {
 
-  if(myDate.isValid()) {
-    res.json({
-      unix: myDate.format("X"),
-      natural: myDate.format("MMMM D, YYYY")
+
+        res.status(200).send({
+          "original_url": theUrl,
+          "short_url": "https://freecodecamp-sandeepscet.c9users.io/" + docs[0].id
+        });
+      }
     });
-  } else {
-    res.json({
-      unix: null,
-      natural: null
-    });
+
+    var id = 0;
+   mongoUtil.mongoGetLastinsertedRecored(function(docs){
+      if(docs.length)
+        id = docs[0].id +1;
+      var doc = {id:id, url : theUrl};
+      // If it's not found, create a new one
+      mongoUtil.mongoInsert(doc, function (doc) {
+      
+        return res.status(200).send({
+          "original_url": theUrl,
+          "short_url": "https://freecodecamp-sandeepscet.c9users.io/" + id
+        });
+      });
+    })
+   
+
+   
   }
-
-
+  else
+  {
+      return res.status(404).send("no Url found");
+  }
 });
 
+app.get('/', function(req, res) {
+  res.status(200).send("Example : localhost:8080/new/https://www.google.com <br> Usage: localhost:8080/2871");
+});
 
 
 
